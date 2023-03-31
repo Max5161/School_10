b221419d038921282b4e3e2011feaa0ea2877e12
Максим <@DESKTOP-G18DDQ2>
2023-03-31 16:01:18 +0500

    Добавьте файлы проекта.

---
+++ School_10/Pages/Ocenki.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace School_10.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Ocenki.xaml
+    /// </summary>
+    public partial class Ocenki : Page
+    {
+        public Ocenki()
+        {
+            InitializeComponent();
+            UpdateOcenki();
+        }
+
+        private void UpdateOcenki()
+        {
+            var Shkola = App.Context.Uroki_Ucheniki.ToList();
+            LviewService.ItemsSource = Shkola; 
+        }
+
+        private void BntSave_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateOcenki();
+        }
+    }
+}
